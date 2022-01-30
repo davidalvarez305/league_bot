19b036b530eee8c4071ba4efb09d931f1d976054
David Alvarez <david@southfloridaathleticclub.com>
2022-01-30 02:38:41 -0500

    switched to node.js cron module over using system cron

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import express from "express";
+import axios from "axios";
 import Discord from "discord.js";
 import { config } from "./config.js";
 import { BOT_PREFIX } from "./constants.js";
 import { Bot } from "./controllers/bot.js";
 import { leagueUsername } from "./utils/bot/leagueUsername.js";
+import { GetTrackedPlayersData } from "./controllers/league.js";
 import { getLastMatchData } from "./actions/bot.js";
+import { CronJob } from "cron";
+import { LEAGUE_ROUTES, API_KEY, CUCU_GUILD_ID } from "./constants.js";
+import { commentary } from "./utils/bot/commentary.js";
 
 const main = async () => {
-  // Middlewares
-  const app = express();
-  app.use(express.json());
 
   // Initialize client
   const discordClient = new Discord.Client({
@@ -21,7 +22,43 @@ const main = async () => {
 
   // Log that the bot has started
   discordClient.on("ready", () => {
-    console.log("The bot is running.");
+    const cronJob = new CronJob("*/20 * * * * *", async () => {
+      console.log('executing cron job...')
+      await GetTrackedPlayersData(async (player) => {
+        const url =
+          LEAGUE_ROUTES.PLAYER_MATCH_HISTORY_BY_PUUID +
+          player.puuid +
+          `/ids?start=0&count=20&api_key=${API_KEY}`;
+        const { data } = await axios.get(url);
+        const lastMatch = data[0];
+        const matchById =
+          LEAGUE_ROUTES.MATCH_BY_ID + lastMatch + `/?api_key=${API_KEY}`;
+        const { data: matchData } = await axios.get(matchById);
+        const rightNow = Date.now();
+        const secondsElapsed =
+          (rightNow - matchData.info.gameEndTimestamp) / 1000;
+        const discordGuild = await discordClient.guilds.fetch(
+          "130528155281653760"
+        );
+        const userData = matchData.info.participants.filter((p) => {
+          return Object.values(p).some((val) =>
+            val.toString().includes(player.userName)
+          );
+        });
+        if (userData && secondsElapsed < 20) {
+          const foundUser = await discordGuild.members.search({
+            query: player.discordUsername,
+          });
+          const discordUser = foundUser.values().next().value.user.id;
+          discordClient.channels
+            .fetch(CUCU_GUILD_ID)
+            .then((channel) =>
+              channel.send(commentary(userData[0], discordUser))
+            );
+        }
+      });
+    });
+    cronJob.start();
   });
 
   // Send a response based on user input
@@ -30,15 +67,17 @@ const main = async () => {
       return;
     } else {
       const command = msg.content.split(BOT_PREFIX)[1].trim();
-      const summonerName = leagueUsername(command)
+      const summonerName = leagueUsername(command);
       switch (true) {
         case command.toLowerCase() === summonerName.userName.toLowerCase(): {
-          const discordGuild = await discordClient.guilds.fetch("130528155281653760");
+          const discordGuild = await discordClient.guilds.fetch(
+            "130528155281653760"
+          );
           const foundUser = await discordGuild.members.search({
             query: summonerName.discordUsername,
           });
           const discordUser = foundUser.values().next().value.user.id;
-          const response = await getLastMatchData(command, discordUser)
+          const response = await getLastMatchData(command, discordUser);
           return msg.reply(response);
         }
         default:
--- scripts/checkLeagueApi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd
-cd league_bot/scripts
-node checkApi.js

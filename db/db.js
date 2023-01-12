a92b484f092015d362c09c70a6e536db986b23bf
david alvarez <david@southfloridaathleticclub.com>
2023-01-12 12:29:19 -0500

    db

--- db/db.js
+++ db/db.js
@@ -7,7 +7,7 @@ import { Participants } from "../models/Participant.js";
 export const AppDataSource = new DataSource({
   type: "postgres",
   username: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DATABASE,
+  database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT,
   host: process.env.POSTGRES_HOST,

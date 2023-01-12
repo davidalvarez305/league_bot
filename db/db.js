921f66c8874e75d0da7a1de37840a2c94a853533
david alvarez <david@southfloridaathleticclub.com>
2023-01-12 12:18:45 -0500

    change in db

--- db/db.js
+++ db/db.js
@@ -6,11 +6,11 @@ import { Participants } from "../models/Participant.js";
 // Initialize PostgreSQL
 export const AppDataSource = new DataSource({
   type: "postgres",
-  username: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
+  username: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
   port: 5432,
-  host: process.env.PGHOST,
+  host: process.env.POSTGRES_HOST,
   synchronize: true,
   entities: [Members, Participants],
 });

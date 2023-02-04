66a6a48a7673579328177d7c6487ceeb6a30d95d
david alvarez <david@southfloridaathleticclub.com>
2023-02-03 23:32:35 -0500

    prompting

--- utils/bot/lastGameCommentary.js
+++ utils/bot/lastGameCommentary.js
@@ -38,34 +38,34 @@ export const lastGameCommentary = (matchData, userName, discordUser) => {
 
   switch (true) {
     case topPlayer && performance.win: {
-      return `$asere text <@${discordUser}> hard carried last game with ${performance.kills} kills, ${performance.assists} assists, ${performance.deaths} deaths. He was the best player in the lobby.`;
+      return `$asere text <@${discordUser}> hard carried last game with ${performance.kills} kills, ${performance.assists} assists, ${performance.deaths} deaths. What can you say about that?`;
     }
     case topPlayer && !performance.win: {
-      return `$asere text <@${discordUser}> was the best player in the last game with ${performance.kills} kills, ${performance.assists} assists, ${performance.deaths} deaths but couldn't bring the dub home. Feels bad man.`;
+      return `$asere text <@${discordUser}> was the best player in the last game with ${performance.kills} kills, ${performance.assists} assists, ${performance.deaths} deaths but couldn't bring the dub home. What can you say about that?`;
     }
     case topDamage && performance.win: {
-      return `$asere text <@${discordUser}> bajo tremendo foco last game with ${performance.kills} kills, ${performance.assists} assists, ${performance.deaths} deaths and got the free dub by doing the most damage. EZ.`;
+      return `$asere text <@${discordUser}> bajo tremendo foco last game with ${performance.kills} kills, ${performance.assists} assists, ${performance.deaths} deaths and got the free dub by doing the most damage. What can you say about that?`;
     }
     case topDamage && !performance.win: {
-      return `$asere text <@${discordUser}> did the most damage last game but couldn't win. Sad days bruv. Man got ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths.`;
+      return `$asere text <@${discordUser}> did the most damage last game but couldn't win. Sad days bruv. Man got ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths What can you say about that?`;
     }
     case topKills && performance.win: {
-      return `$asere text <@${discordUser}> just got ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths. El tipo le dio caña a to el mundo & got the DUB.`;
+      return `$asere text <@${discordUser}> just got ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths. El tipo le dio caña a to el mundo & got the DUB. What can you say about that?`;
     }
     case topKills && !performance.win: {
-      return `$asere text <@${discordUser}> just got ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths. Killed a lot of people but didn't win. XD. Chased too much.`;
+      return `$asere text <@${discordUser}> just got ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths. Killed a lot of people but didn't win. What can you say about that?`;
     }
     case leastDamage && performance.win: {
-      return `$asere text <@${discordUser}> did the least damage in the game but somehow got carried and won. XD. Man had ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths. I wish I was you bruv.`;
+      return `$asere text <@${discordUser}> did the least damage in the game but somehow got carried and won. XD. Man had ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths. What can you say about that?`;
     }
     case leastDamage && !performance.win: {
-      return `$asere text <@${discordUser}> did the least damage and caused his team the game. ALT-F4 is the name of the game for you. LOL. He got ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths.`;
+      return `$asere text <@${discordUser}> did the least damage and caused his team the game. ALT-F4 is the name of the game for you. LOL. He got ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths. What can you say about that?`;
     }
     case worstPlayer && performance.win: {
-      return `$asere text <@${discordUser}> IDK how you managed to be that bad and still win. Man was the worst player in the lobby and got the dub by getting carried. He got ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths.`;
+      return `$asere text <@${discordUser}> IDK how you managed to be that bad and still win. Man was the worst player in the lobby and got the dub by getting carried. He got ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths. What can you say about that?`;
     }
     case worstPlayer && !performance.win: {
-      return `$asere text <@${discordUser}> not only didn't you not win, but you were also the worst player in the lobby with ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths. LMAO. Aye...trust ALT-F4. Maybe even uninstall, idk.`;
+      return `$asere text <@${discordUser}> not only didn't you not win, but you were also the worst player in the lobby with ${performance.kills} kills, ${performance.assists} assists, and ${performance.deaths} deaths. What can you say about that?`;
     }
     default: {
       return `$asere text <@${discordUser}> just got ${performance.kills} kills, ${
@@ -73,8 +73,8 @@ export const lastGameCommentary = (matchData, userName, discordUser) => {
       } assists, and ${
         performance.deaths
       } deaths. Nothing special, average player.${
-        performance.win ? ` Got carried.` : ` Lost because he stank.`
-      }`;
+        performance.win ? ` Got carried.` : ` Lost because he sucks.`
+      } What can you say about that?`;
     }
   }
 };
